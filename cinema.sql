b55affc94991c846ffa0c39c1dbc21b2cb3f9cd2
Juliana Ferreira <julianafercavalcante@hotmail.com>
2022-12-17 02:40:18 -0300

    finalização do projeto

--- cinema.sql
+++ cinema.sql
@@ -142,7 +142,8 @@ create table personagem
     genero varchar(1) not null,
     constraint PK_personagem primary key (numero, filme_codigo),
     constraint FK_personagem_filme foreign key (filme_codigo) references filme (codigo),
-    constraint CK_numero_personagem check(numero>0)
+    constraint CK_numero_personagem check(numero>0),
+    constraint CK_genero_personagem check(genero in ('f','m'))
 );
 
 /*criaçao da tabela genero*/
@@ -195,3 +196,105 @@ insert into cliente values ('65865438743', 'Rafaela Alves');
 insert into cliente values ('11111111111', 'Arthur Oliveira');
 insert into cliente values ('47532154376', 'Carla Maria');
 insert into cliente values ('54734587900', 'Lívia Martins');
+insert into cliente values ('12345678976', 'Ricardo Luiz');
+
+/*inserindo dados na tabela venda*/
+insert into venda values (123, '2022-05-28', '11:15:25', '12345678901', '11111111111'); # Lucas Rangel vendeu para Arthur Oliveira
+insert into venda values (325, '2022-11-02', '09:10:25', '45677898766', '54734587900'); # Mel Maia vendeu para Lívia Martins
+insert into venda values (873, '2022-09-30', '20:04:07', '65473289337', '87514231876'); # Christian Figueiredo vendeu para Thomas Lima
+insert into venda values (404, '2021-01-21', '00:06:22', '52435698435', '47532154376'); # Gessica Kayane vendeu para Carla Maria
+insert into venda values (762, '2021-10-09', '15:15:15', '54801375209', '65865438743'); # Carlinhos Maia vendeu para Rafaela Alves
+insert into venda values (234, '2022-12-16', '19:30:45', '54801375209', '12345678976'); # Carlinhos Mais vendeu para Ricardo Luiz
+
+/*inserindo dados na tabela filme*/
+insert into filme values (31, 'Ricos de Amor', 'd'); /*default não tá funcionando*/
+insert into filme values (12, 'Crônicas de natal', 'l');
+insert into filme values (73, 'Vingadores', 'd'); /*default não tá funcionando*/
+insert into filme values (45, 'Minha mãe é uma peça', 'd'); /*default não tá funcionando*/
+insert into filme values (25, 'Coringa', 'l');
+
+/*inserindo dados na tabela sala*/
+insert into sala values (1, 'n');
+insert into sala values (2, 'n');
+insert into sala values (3, 'n');
+insert into sala values (4, 'n');
+insert into sala values (5, 'p');
+
+/*inserindo dados na tabela sessão*/
+insert into sessao values (1234, 1, 25, '2021-11-09', '00:00:00'); # Coringa na sala 1
+insert into sessao values (7652, 2, 44, '2021-12-31', '10:55:00'); # Minha mãe é uma peça na sala 2
+insert into sessao values (1745, 3, 73, '2022-02-05', '16:00:00'); # Vingadores na sala 3
+insert into sessao values (9786, 4, 12, '2022-07-08', '20:30:00'); # Crônicas de natal na sala 4
+insert into sessao values (8888, 5, 31, '2022-08-07', '13:45:00'); # Ricos de Amor na sala 5
+
+/*inserindo dados na tabela ingresso*/
+insert into ingresso values (87654, 10.00, 123, 1234); # estudante
+insert into ingresso values (54321, 10.00, 325, 7652); # estudante
+insert into ingresso values (23456, 10.00, 873, 1745); # estudante
+insert into ingresso values (56437, 10.00, 404, 9786); # estudante
+insert into ingresso values (65421, 15.00, 762, 8888); # estudante
+insert into ingresso values (34132, 30.00, 762, 8888);
+insert into ingresso values (57898, 20.00, 325, 7652);
+
+/*inserindo dados na tabela estudante*/
+insert into estudante values (87654, null);
+insert into estudante values (54321, null);
+insert into estudante values (23456, null);
+insert into estudante values (56437, null);
+insert into estudante values (65421, null);
+
+/*inserindo dados na tabela personagem*/
+insert into personagem values (11, 31, 'Paula', 'f'); # Personagem paula do filme Ricos de Amor
+insert into personagem values (22, 12, 'Papai Noel', 'm'); # Personagem Papai Noel do filme Crônicas de Natal
+insert into personagem values (33, 73, 'Thanos', 'm'); # Personagem Thanos do filme Vingadores
+insert into personagem values (44, 45, 'Hermínia', 'f'); # Personagem Hermínia do filme Minha mãe é uma peça
+insert into personagem values (55, 25, 'Coringa', 'm'); # Personagem Coringa do filme Coringa
+
+/*inserindo dados na tabela genero*/
+insert into genero values ('Romance', 31);
+insert into genero values ('Ficção', 12);
+insert into genero values ('Aventura', 73);
+insert into genero values ('Comédia', 45);
+insert into genero values ('Drama', 25);
+
+/*inserindo dados na tabela poltrona*/
+insert into poltrona values (1, 1, 'a');
+insert into poltrona values (2, 3, 'b');
+insert into poltrona values (3, 5, 'a');
+insert into poltrona values (4, 7, 'f');
+insert into poltrona values (5, 9, 'a');
+
+/* CONSULTA DE DADOS */
+
+/*Obter nome e email dos funcionários com sobre nome Maia, ordenando pelo nome do funcionário em ordem descente*/
+select f.nome as 'Funcionário',
+	   e.email as 'E-mail'
+from email as e
+join funcionario as f
+	 on e.funcionario_cpf = f.cpf
+having f.nome like '%Maia'
+order by f.nome desc;
+
+/*Obter a quantidade de funcionários que possuem condomínio e que nasceram depois dos anos 1990 de cada genero*/
+select genero as 'Gênero',
+	   COUNT(*) AS Quantidade
+from funcionario
+where condominio is not null and
+	  ano_nasc between 1990 and 2004
+group by genero;
+
+/*Obter o nome dos filmes que serão passados nas salas de número ímpar*/
+select s.sala_numero as 'Número da sala',
+       f.nome as 'Filme'
+from sessao as s
+join filme as f
+	 on s.filme_codigo = f.codigo
+where s.sala_numero in (1,3,5)
+order by s.sala_numero asc;
+
+/*Obter horário e filme da sessão mais cedo do cinema*/
+select min(s.hora) as 'Horário',
+	   f.nome as 'Filme'
+from sessao as s
+join filme as f
+	 on s.filme_codigo = f.codigo;
